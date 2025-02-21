ced15edce859a50c3f84ed09188326509af6ae99
Pin-Xuan She <snakeneedy@gmail.com>
2025-02-21 14:26:52 +0800

    Fix PORT

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import http from 'http';
 import url from 'url';
 import routes from './routes/index.js';
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const method = req.method.toUpperCase();
